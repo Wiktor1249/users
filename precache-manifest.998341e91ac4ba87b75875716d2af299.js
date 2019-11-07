0079dade11513fa5dfeba0c860f3b69163d1a1c9
Wiktor1249 <wiktor1249@wp.pl>
2019-11-07 09:08:29 +0100

    Updates

---
+++ precache-manifest.998341e91ac4ba87b75875716d2af299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3512dcbde643c765dc859df44420cc",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "531ee4541bb44bfdcae1",
+    "url": "/users/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f987ba54d7f8a7512792",
+    "url": "/users/static/js/2.6edbfc48.chunk.js"
+  },
+  {
+    "revision": "531ee4541bb44bfdcae1",
+    "url": "/users/static/js/main.7e177467.chunk.js"
+  },
+  {
+    "revision": "1fe9f15e4f2571e75ea0",
+    "url": "/users/static/js/runtime-main.8ecbba6d.js"
+  }
+]);
